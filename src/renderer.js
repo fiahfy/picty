a26ef094863721af7c20cbe628ee44d606cd7b45
fiahfy <d.fiahfy@gmail.com>
2017-09-07 19:46:05 +0900

    Add vuex

--- src/renderer.js
+++ src/renderer.js
@@ -1,14 +1,19 @@
 import Vue from 'vue';
+import { sync } from 'vuex-router-sync';
 import App from './renderer/App';
 import router from './renderer/router';
+import store from './renderer/store';
+import './renderer/mixin';
 
 Vue.config.devtools = false;
 Vue.config.productionTip = false;
 
+sync(store, router);
 
 new Vue({ // eslint-disable-line no-new
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App },
 });
