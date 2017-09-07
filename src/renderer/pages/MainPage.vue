7a8dad2e4ef348d2d42c2c9d4c55ad4a40d550c0
fiahfy <d.fiahfy@gmail.com>
2017-09-07 20:42:00 +0900

    Add dummy state

--- src/renderer/pages/MainPage.vue
+++ src/renderer/pages/MainPage.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
     main page
+    <span v-for="file in files" :key="file">
+      {{ file }}
+    </span>
   </div>
 </template>
 
 <script>
 export default {
   name: 'main-page',
+  asyncData ({ store, route }) {
+    return store.dispatch('readDir', '')
+  },
+  computed: {
+    files() {
+      return this.$store.state.files
+    }
+  }
 };
 </script>
 
--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -3,11 +3,28 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+function readDir(dir) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(['a', 'b', 'c']);
+    }, 3000);
+  });
+}
+
 export default new Vuex.Store({
   state: {
+    files: [],
   },
   actions: {
+    readDir({ commit }, dir) {
+      return readDir(dir).then((files) => {
+        commit('setFiles', { files });
+      });
+    },
   },
   mutations: {
+    setFiles(state, { files }) {
+      Vue.set(state, 'files', files);
+    },
   },
 });
