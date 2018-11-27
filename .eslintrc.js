323df4ab5ffaf73ec90d37ba2781713e4dc8d9d6
fiahfy <d.fiahfy@gmail.com>
2018-11-27 13:38:46 +0900

    yarn upgrade

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,14 +10,19 @@ module.exports = {
   ],
   rules: {
     'vue/max-attributes-per-line': 'off',
-    'vue/html-self-closing': ['error', {
-      'html': {
-        'void': 'always',
-        'normal': 'always',
-        'component': 'always'
-      },
-      'svg': 'always',
-      'math': 'always'
-    }]
+    'vue/component-name-in-template-casing': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always'
+        },
+        svg: 'always',
+        math: 'always'
+      }
+    ]
   }
 }
